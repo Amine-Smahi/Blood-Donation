7772dcd0ead25106e3a4d1a2ce657694e5564861
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-12-14 20:41:02 +0100

    Small fixes and add profile activity

--- app/src/main/java/com/jetlight/amine_smahi/inssen/DonnorActivity.java
+++ app/src/main/java/com/jetlight/amine_smahi/inssen/DonnorActivity.java
@@ -1,6 +1,8 @@
 package com.jetlight.amine_smahi.inssen;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -60,6 +62,17 @@ public class DonnorActivity extends AppCompatActivity {
             R.mipmap.avatar2,
     };
 
+    public void CallHim(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0123456789"));
+        startActivity(intent);
+    }
+    public void ViewProfile(View view)
+    {
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
